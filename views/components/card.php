cbec4140f6945db6a665ce461de93547a80fcd65
Lucas Ferreira <ferreiralucas340@outlook.com>
2026-08-20 21:39:41 -0300

    feat: Adicionado o componente de cards de produtos e o layout da página inicial, com os estilos e recursos associados.

---
+++ views/components/card.php
@@ -0,0 +1,31 @@
+
+<div class="card">
+    <div class="card-image">
+        <img src="../assets/image/products/relogio.jpg" alt="Relogio" width="300">
+    </div>
+
+    <div class="card-content">
+        <h4 class="card-title">Titulo do Produto</h4>
+
+        <div>
+            <span class="card-stars">
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+            </span>
+            <span class="stars-count">(128)</span>
+        </div>
+
+        <div class="card-price">
+            <span class="price">R$ 198,90</span>
+            <span class="discount">R$ 249,90</span>   
+        </div>
+
+        <a href="#" class="btn-card">
+            <i class="fa-solid fa-cart-shopping"></i>
+            <span>Adicionar ao Carrinho</span>
+        </a>
+    </div>
+</div>
--- views/home/index.php
+++ views/home/index.php
@@ -1,4 +1,50 @@
-<section id="home">
+<?php
 
+use App\Core\View;
 
-</section>
+?>
+
+<div id="home">
+    <section id="offers">
+        <div class="content-offers">
+            <div class="container-content">
+                <div class="badge-offers bg-accent">
+                    <span class="text-white">coleção de inverno 2026</span>
+                </div>
+
+                <h1 class="text-white">Estilo que fala por você</h1>
+
+                <P class="text-white">Até 40% de desconto em peças selecionadas. Só esta semana.</P>
+
+                <a href="#" class="bg-accent text-white">Ver Ofertas</a>
+            </div>
+        </div>
+    </section>
+
+    <section id="container-main">
+        <!-- FILTRO -->
+        <div class="filter-links">
+            <a href="?todos" class="active">Todos</a>
+            <a href="?calcados">Calçados</a>
+            <a href="?roupas">Roupas</a>
+            <a href="?acessorios">Acessórios</a>
+            <a href="?eletronicos">Eletrônicos</a>
+            <a href="?bolsas">Bolsas</a>
+        </div>
+
+        <div id="container-products">
+            <div class="header-products">
+                <h2>Produtos em Destaque</h2>
+                <a href="#">Ver todos</a>
+            </div>
+
+            <div class="container-cards">
+                <?php View::component('card'); ?>
+                <?php View::component('card'); ?>
+                <?php View::component('card'); ?>
+                <?php View::component('card'); ?>
+                <?php View::component('card'); ?>
+            </div>
+        </div>
+    </section>
+</div>
